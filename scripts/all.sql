7d00aeeb12ee276e803173a7c08015cf77f1c3e6
Mordecaikama <agyapongmordecai@gmail.com>
2025-06-09 09:15:33 +0100

    updated all sql file

--- scripts/all.sql
+++ scripts/all.sql
@@ -125,4 +125,132 @@ select
 from
 	yanki.products;
 
-select * from yanki.orders limit 10;
+
+-- inner join
+-- retrieve customer etails alon with the products they ordered.
+select
+	c.customer_id,
+	c.customer_name,
+	c.email,
+	c.phone_number,
+	od.order_id,
+	p.product_id,
+	p.product_name,
+	p.Brand,
+	p.price,
+	od.quantity,
+	od.total_price
+from
+	yanki.customers as c
+inner join
+	yanki.orders as od on c.customer_id = od.customer_id
+inner join
+	yanki.products as p on od.product_id = p.product_id;
+
+-- retrieve order details along with payment information.
+
+select
+	od.order_id,
+	pm.payment_method,
+	pm.transaction_status
+from
+	yanki.orders as od
+inner join
+	yanki.payment_method pm on od.order_id = pm.order_id;
+
+
+
+-- inner join is when the tables match exactly
+-- left join returns all data from left handside and matches that of the secod table
+-- right join returns all data from right handside and matches that of the secod table
+
+
+
+-- left join;
+-- retrieve all customers along with their orers, even if they havent placed any orders.
+
+select 
+	c.customer_id,
+	c.customer_name,
+	c.email,
+	c.phone_number,
+	od.order_id,
+	od.product_id,
+	od.quantity,
+	od.total_price
+from
+	yanki.customers as c
+left join
+	yanki.orders as od on c.customer_id = od.customer_id;
+
+-- retrieve all orders along with product details, even if there are no rcorrespond products.
+
+select
+	od.order_id,
+	od.customer_id,
+	p.product_id,
+	p.product_name,
+	p.price,
+	od.quantity,
+	od.total_price
+from
+	yanki.orders as od
+left join
+	yanki.products as p on od.product_id = p.product_id;
+
+-- right join
+-- retireve all orer along with payment information, even if there a re no correaponding payment records.
+
+select
+	od.order_id,
+	pm.payment_method,
+	pm.transaction_status
+from 
+	yanki.orders as od
+right join
+	yanki.payment_method pm on od.order_id = pm.order_id;
+
+-- retireve all products along with the orders, even if there are no correspiding orders.
+select
+	p.product_id,
+	p.product_name,
+	od.order_ID,
+	od.quantity,
+	od.total_price
+from
+	yanki.products as p
+right join
+	yanki.orders as od on p.product_id = od.product_id;
+
+-- outer join
+-- retrieve all customers along with their orders, including customer who have not placed any orders,
+-- and  order
+
+select
+	c.customer_id,
+	c.customer_name,
+	c.email,
+	c.phone_number,
+	od.order_id,
+	od.product_id,
+	od.quantity,
+	od.total_price
+from
+	yanki.customers c
+full outer join
+	yanki.orders as od on c.customer_id = od.customer_id;
+
+-- retireve all orders alon g with payment information, including orders without corresponding
+-- payment record and 
+
+select
+	od.order_id,
+	pm.payment_method,
+	pm.transaction_status
+from
+	yanki.orders as od
+full outer join
+	yanki.payment_method as pm on od.order_id = pm.order_id;
+
+
+select * from yanki.payment_method limit 10;
